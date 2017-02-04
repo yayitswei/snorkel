9e7971cc2d50fd4fa866fb28ba7dbd5228c338f6
Braden Hancock <hancock.braden@gmail.com>
2017-02-03 17:46:30 -0800

    Reorder classes in grammar.py

--- snorkel/semantic/grammar.py
+++ snorkel/semantic/grammar.py
@@ -7,78 +7,6 @@ from types import FunctionType
 
 from util import CoreNLPHandler
 
-# Rule =========================================================================
-
-class Rule(object):
-    """Represents a CFG rule with a semantic attachment."""
-
-    def __init__(self, lhs, rhs, sem=None):
-        self.lhs = lhs
-        self.rhs = tuple(rhs.split()) if isinstance(rhs, str) else rhs
-        self.sem = sem
-        self.validate_rule()
-
-    def __str__(self):
-        """Returns a string representation of this Rule."""
-        return 'Rule' + str((self.lhs, ' '.join(self.rhs), self.sem))
-
-    def apply_semantics(self, sems):
-        # Note that this function would not be needed if we required that semantics
-        # always be functions, never bare values.  That is, if instead of
-        # Rule('$E', 'one', 1) we required Rule('$E', 'one', lambda sems: 1).
-        # But that would be cumbersome.
-        if isinstance(self.sem, FunctionType):
-            return self.sem(sems)
-        else:
-            return self.sem
-
-    def is_lexical(self):
-        """
-        Returns true iff the given Rule is a lexical rule, i.e., contains only
-        words (terminals) on the RHS.
-        """
-        return all([not is_cat(rhsi) for rhsi in self.rhs])
-
-    def is_unary(self):
-        """
-        Returns true iff the given Rule is a unary compositional rule, i.e.,
-        contains only a single category (non-terminal) on the RHS.
-        """
-        return len(self.rhs) == 1 and is_cat(self.rhs[0])
-
-    def is_binary(self):
-        """
-        Returns true iff the given Rule is a binary compositional rule, i.e.,
-        contains exactly two categories (non-terminals) on the RHS.
-        """
-        return len(self.rhs) == 2 and is_cat(self.rhs[0]) and is_cat(self.rhs[1])
-
-    def validate_rule(self):
-        """Returns true iff the given Rule is well-formed."""
-        assert is_cat(self.lhs), 'Not a category: %s' % self.lhs
-        assert isinstance(self.rhs, tuple), 'Not a tuple: %s' % self.rhs
-        for rhs_i in self.rhs:
-            assert isinstance(rhs_i, basestring), 'Not a string: %s' % rhs_i
-
-    def contains_optionals(self):
-        """Returns true iff the given Rule contains any optional items on the RHS."""
-        return any([is_optional(rhsi) for rhsi in self.rhs])
-
-def is_cat(label):
-    """
-    Returns true iff the given label is a category (non-terminal), i.e., is
-    marked with an initial '$'.
-    """
-    return label.startswith('$')
-
-def is_optional(label):
-    """
-    Returns true iff the given RHS item is optional, i.e., is marked with an
-    initial '?'.
-    """
-    return label.startswith('?') and len(label) > 1
-
-
 # Grammar ======================================================================
 
 class Grammar(object):
@@ -285,6 +213,78 @@ class Grammar(object):
                 print(self.chart[span][0])
                 for entry in self.chart[span][1:]:
                     print('%-12s' % ' ', entry)
+
+# Rule =========================================================================
+
+class Rule(object):
+    """Represents a CFG rule with a semantic attachment."""
+
+    def __init__(self, lhs, rhs, sem=None):
+        self.lhs = lhs
+        self.rhs = tuple(rhs.split()) if isinstance(rhs, str) else rhs
+        self.sem = sem
+        self.validate_rule()
+
+    def __str__(self):
+        """Returns a string representation of this Rule."""
+        return 'Rule' + str((self.lhs, ' '.join(self.rhs), self.sem))
+
+    def apply_semantics(self, sems):
+        # Note that this function would not be needed if we required that semantics
+        # always be functions, never bare values.  That is, if instead of
+        # Rule('$E', 'one', 1) we required Rule('$E', 'one', lambda sems: 1).
+        # But that would be cumbersome.
+        if isinstance(self.sem, FunctionType):
+            return self.sem(sems)
+        else:
+            return self.sem
+
+    def is_lexical(self):
+        """
+        Returns true iff the given Rule is a lexical rule, i.e., contains only
+        words (terminals) on the RHS.
+        """
+        return all([not is_cat(rhsi) for rhsi in self.rhs])
+
+    def is_unary(self):
+        """
+        Returns true iff the given Rule is a unary compositional rule, i.e.,
+        contains only a single category (non-terminal) on the RHS.
+        """
+        return len(self.rhs) == 1 and is_cat(self.rhs[0])
+
+    def is_binary(self):
+        """
+        Returns true iff the given Rule is a binary compositional rule, i.e.,
+        contains exactly two categories (non-terminals) on the RHS.
+        """
+        return len(self.rhs) == 2 and is_cat(self.rhs[0]) and is_cat(self.rhs[1])
+
+    def validate_rule(self):
+        """Returns true iff the given Rule is well-formed."""
+        assert is_cat(self.lhs), 'Not a category: %s' % self.lhs
+        assert isinstance(self.rhs, tuple), 'Not a tuple: %s' % self.rhs
+        for rhs_i in self.rhs:
+            assert isinstance(rhs_i, basestring), 'Not a string: %s' % rhs_i
+
+    def contains_optionals(self):
+        """Returns true iff the given Rule contains any optional items on the RHS."""
+        return any([is_optional(rhsi) for rhsi in self.rhs])
+
+def is_cat(label):
+    """
+    Returns true iff the given label is a category (non-terminal), i.e., is
+    marked with an initial '$'.
+    """
+    return label.startswith('$')
+
+def is_optional(label):
+    """
+    Returns true iff the given RHS item is optional, i.e., is marked with an
+    initial '?'.
+    """
+    return label.startswith('?') and len(label) > 1
+
     
 # Parse ========================================================================
 

12b542d1df085cbd3946d729a451639d44e510fd
Braden Hancock <hancock.braden@gmail.com>
2017-02-09 16:43:10 -0800

    Annotators return rules (for gobbling optionals)

--- snorkel/semantic/annotator.py
+++ snorkel/semantic/annotator.py
@@ -1,12 +1,28 @@
+from grammar import Rule
+
 import re
 
 class Annotator:
     """A base class for annotators."""
+    def __init__(self):
+        self.rules = self.make_rules()
+
     def annotate(self, tokens):
         """Returns a list of pairs, each a category and a semantic representation."""
         return []
+    
+    def make_rules(self):
+        """Returns a list of lexical rules"""
+        rules = []
+        for lhs in self.categories:
+            rules.append(Rule(lhs, (lhs, '$Token'), lambda sems: sems[0]))
+        return rules
 
 class TokenAnnotator(Annotator):
+    def __init__(self):
+        self.categories = []
+        Annotator.__init__(self)
+    
     def annotate(self, tokens):
         if len(tokens) == 1:
             return [('$Token', tokens[0]['word'])]
@@ -14,6 +30,10 @@ class TokenAnnotator(Annotator):
             return []
 
 class PunctuationAnnotator(Annotator):
+    def __init__(self):
+        self.categories = ['$OpenQuote', '$CloseQuote', '$OpenParen', '$CloseParen']
+        Annotator.__init__(self)
+    
     def annotate(self, tokens):
         if len(tokens) == 1:
             if tokens[0]['pos'] == "``":
@@ -27,6 +47,10 @@ class PunctuationAnnotator(Annotator):
         return []
 
 class IntegerAnnotator(Annotator):
+    def __init__(self):
+        self.categories = ['$Int']
+        Annotator.__init__(self)
+
     def annotate(self, tokens):
         if len(tokens) == 1:
             if all(token['ner'] in ['NUMBER','ORDINAL'] for token in tokens):

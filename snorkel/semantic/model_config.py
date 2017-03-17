9eeb80081cda15520b4c458c4e2dff0d30ba10d3
Braden Hancock <hancock.braden@gmail.com>
2017-03-16 18:02:02 -0700

    Begin tracking model_config file

---
+++ snorkel/semantic/model_config.py
@@ -0,0 +1,31 @@
+configuration = {
+    # general
+    'db': None,
+    'parallelism': 1,
+    'max_docs': 1500,
+    'splits': 3,
+    'verbose': True,
+
+    # Supervision
+    'source': 'py',
+    'include': [],
+    'max_lfs': None,
+    'max_train': None,
+    'model_dep': False,
+    'majority_vote': False,
+    'traditional': False,
+    'threshold': 1.0/3.0,
+    'display_correlation': False,
+    'display_marginals': False,
+    'empirical_from_train': False,
+
+    # Classification,
+    'model': 'logreg',
+    'n_search': 10,
+    'n_epochs': 5,
+    'rebalance': True,
+    'lr': None,
+    'l1_penalty': None,
+    'l2_penalty': None,
+    'print_freq': 5,
+}

b71079b9c99c9a5e46b42e6054596ef8ac41bddf
Braden Hancock <hancock.braden@gmail.com>
2017-03-24 08:55:18 -0700

    Add inverted functions, replace stopwords

--- snorkel/semantic/ricky.py
+++ snorkel/semantic/ricky.py
@@ -16,6 +16,14 @@ def sems_in_order(sems):
 def sems_reversed(sems):
     return tuple(reversed(sems))
 
+def flip_dir(dir_):
+    if dir_ == '.right':
+        return '.left'
+    elif dir_ == '.left':
+        return '.right'
+    else:
+        raise Exception
+
 # Rules ======================================================================
 lexical_rules = (
     [Rule('$Start', w) for w in ['<START>']] +
@@ -30,7 +38,6 @@ lexical_rules = (
     [Rule('$All', w, '.all') for w in ['all']] +
     [Rule('$Any', w, '.any') for w in ['any', 'a']] +
     [Rule('$None', w, '.none') for w in ['none', 'not any', 'neither', 'no']] +
-    # [Rule('$Is', w) for w in ['is', 'are', 'be']] +
     [Rule('$Because', w) for w in ['because', 'since', 'if']] +
     [Rule('$Upper', w, '.upper') for w in ['upper', 'uppercase', 'upper case', 'all caps', 'all capitalized']] +
     [Rule('$Lower', w, '.lower') for w in ['lower', 'lowercase', 'lower case']] +
@@ -41,19 +48,20 @@ lexical_rules = (
     [Rule('$AtLeast', w, '.geq') for w in ['at least', 'no less than', 'no smaller than', 'greater than or equal', '>=']] +
     [Rule('$MoreThan', w, '.gt') for w in ['more than', 'greater than', 'larger than', '>']] + 
     [Rule('$Within', w, '.within') for w in ['within']] +
-    [Rule('$Exists', w) for w in ['exist', 'exists', 'there']] +
+    [Rule('$Exists', w) for w in ['exist', 'exists']] +
     [Rule('$Int', w, ('.int', 0)) for w in ['no']] +
-    [Rule('$Int', w,  ('.int', 1)) for w in ['immediately']] +
+    [Rule('$Int', w,  ('.int', 1)) for w in ['immediately', 'right']] +
+    [Rule('$Is', w) for w in ['is', 'are', 'be', 'comes', 'appears', 'occurs']] +
     # Rule('$AtLeastOne', 'a', ('.geq', ('.int', 1))),
     # Rule('$Int', 'a', ('.int', 1)),
     [Rule('$In', w, '.in') for w in ['in']] +
-    [Rule('$Contains', w, '.contains') for w in ['contains', 'contain']] +
-    [Rule('$StartsWith', w, '.startswith') for w in ['starts with', 'start with']] +
-    [Rule('$EndsWith', w, '.endswith') for w in ['ends with', 'end with']] +
+    [Rule('$Contains', w, '.contains') for w in ['contains', 'contain', 'containing']] +
+    [Rule('$StartsWith', w, '.startswith') for w in ['starts with', 'start with', 'starting with']] +
+    [Rule('$EndsWith', w, '.endswith') for w in ['ends with', 'end with', 'ending with']] +
     [Rule('$Left', w, '.left') for w in ['left', 'before', 'precedes', 'preceding', 'followed by']] +
-    [Rule('$Right', w, '.right') for w in ['right', 'after', 'follows', 'following']] +
-    [Rule('$Sentence', w, '.sentence') for w in ['sentence', 'in the sentence']] +
-    [Rule('$Between', w, '.between') for w in ['between', 'inbetween']] +
+    [Rule('$Right', w, '.right') for w in ['right', 'after', 'preceded by', 'follows', 'following']] +
+    [Rule('$Sentence', w, '.sentence') for w in ['sentence']] +
+    [Rule('$Between', w, '.between') for w in ['between', 'inbetween', 'sandwiched', 'enclosed']] +
     [Rule('$Separator', w) for w in [',', ';', '/']] +
     [Rule('$Count', w, '.count') for w in ['number', 'length', 'count']] +
     [Rule('$Word', w, 'words') for w in ['word', 'words', 'term', 'terms', 'phrase', 'phrases']] + 
@@ -69,7 +77,8 @@ lexical_rules = (
     # FIXME: Temporary hardcode
     [Rule('$ChemicalEntity', w, ('.string', 'Chemical')) for w in ['chemical', 'chemicals']] +
     [Rule('$DiseaseEntity', w, ('.string', 'Disease')) for w in ['disease', 'diseases']] +
-    [Rule('$CID', w, '.cid') for w in ['cid', 'cids', 'canonical id', 'canonical ids']]
+    [Rule('$CID', w, '.cid') for w in ['cid', 'cids', 'canonical id', 'canonical ids']] +
+    [Rule('$ArgXAnd', w, ('.list', ('.arg', ('.int', 1)), ('.arg', ('.int', 2)))) for w in ['them']]
     # FIXME
 )
 
@@ -83,7 +92,7 @@ unary_rules = [
     Rule('$BoolLit', '$False', sems0),
     Rule('$Conj', '$And', sems0),
     Rule('$Conj', '$Or', sems0),
-    # Rule('$Exists', '$Is'),
+    Rule('$Exists', '$Is'),
     # Rule('$Equals', '$Is', '.eq'),
     Rule('$Compare', '$Equals', sems0),
     Rule('$Compare', '$NotEquals', sems0),
@@ -111,7 +120,7 @@ unary_rules = [
     Rule('$BinaryStringToBool', '$EndsWith', sems0),
     Rule('$BinaryStringToBool', '$In', sems0),
     Rule('$BinaryStringToBool', '$Contains', sems0),
-    Rule('$BinaryStringToBool', '$Compare', sems0),
+    Rule('$BinaryStringToBool', '$Equals', sems0),
     Rule('$IntToBool', '$AtLeastOne', sems0),
     # ArgX may be treated as an object or a string (referring to its textual contents)
     Rule('$String', '$ArgX', lambda sems: ('.arg_to_string', sems[0])),
@@ -135,17 +144,24 @@ compositional_rules = [
     Rule('$Bool', '$All $BoolList', sems_in_order),
     Rule('$Bool', '$Any $BoolList', sems_in_order),
     Rule('$Bool', '$None $BoolList', sems_in_order),
-    
+    Rule('$Bool', '$BoolList', lambda (boollist_,): ('.any', boollist_)),
+
     # Parentheses
     Rule('$Bool', '$OpenParen $Bool $CloseParen', lambda (open_, bool_, close_): bool_),
     # Rule('$StringListAnd', '$OpenParen $StringListAnd $CloseParen', lambda (open_, list_, close_): list_),
     # Rule('$StringListOr', '$OpenParen $StringListOr $CloseParen', lambda (open_, list_, close_): list_),
 
     ### Strings ###
-        # building strings
-    Rule('$StringStub', '$Quote $QueryToken', lambda sems: [sems[1]]),
-    Rule('$StringStub', '$StringStub $QueryToken', lambda sems: sems[0] + [sems[1]]),
-    Rule('$String', '$StringStub $Quote', lambda sems: ('.string', ' '.join(sems[0]))),
+        # building strings of arbitrary length
+    # Rule('$StringStub', '$Quote $QueryToken', lambda sems: [sems[1]]),
+    # Rule('$StringStub', '$StringStub $QueryToken', lambda sems: sems[0] + [sems[1]]),
+    # Rule('$String', '$StringStub $Quote', lambda sems: ('.string', ' '.join(sems[0]))),
+        # building strings of max length 5
+    Rule('$String', '$Quote $QueryToken $Quote', lambda sems: ('.string', ' '.join(sems[1:2]))),
+    Rule('$String', '$Quote $QueryToken $QueryToken $Quote', lambda sems: ('.string', ' '.join(sems[1:3]))),
+    Rule('$String', '$Quote $QueryToken $QueryToken $QueryToken $Quote', lambda sems: ('.string', ' '.join(sems[1:4]))),
+    Rule('$String', '$Quote $QueryToken $QueryToken $QueryToken $QueryToken $Quote', lambda sems: ('.string', ' '.join(sems[1:5]))),
+    Rule('$String', '$Quote $QueryToken $QueryToken $QueryToken $QueryToken $QueryToken $Quote', lambda sems: ('.string', ' '.join(sems[1:6]))),
 
         # building string lists (TODO: remove some redundancies here?)
     Rule('$StringListStub', '$String ?$Separator $String', lambda sems: ('.list', sems[0], sems[2])),
@@ -158,11 +174,22 @@ compositional_rules = [
     Rule('$StringListAnd', '$StringListStub ?$Separator $And $String', lambda sems: tuple(list(sems[0]) + [sems[3]])),
 
         # applying $StringToBool functions
-    Rule('$Bool', '$String $StringToBool', lambda sems: ('.call', sems[1], sems[0])),
+    Rule('$Bool', '$String $StringToBool', lambda (str_, func_): ('.call', func_, str_)),
     Rule('$Bool', '$String $Not $StringToBool', lambda (str_, not_, func_): (not_, ('.call', func_, str_))),
-    Rule('$Bool', '$StringListOr $StringToBool', lambda sems: ('.any', ('.map', sems[1], sems[0]))),
-    Rule('$Bool', '$StringListAnd $StringToBool', lambda sems: ('.all', ('.map', sems[1], sems[0]))),
-    Rule('$BoolList', '$StringList $StringToBool', lambda sems: ('.map', sems[1], sems[0])),
+    Rule('$Bool', '$StringListOr $StringToBool', lambda (strlist_, func_): ('.any', ('.map', func_, strlist_))),
+    Rule('$Bool', '$StringListAnd $StringToBool', lambda (strlist_, func_): ('.all', ('.map', func_, strlist_))),
+    Rule('$BoolList', '$StringList $StringToBool', lambda (strlist_, func_): ('.map', func_, strlist_)),
+    Rule('$Bool', '$Exists $StringList $StringToBool', lambda (exists_, strlist_, func_): ('.any', ('.map', func_, strlist_))),
+    Rule('$Bool', '$StringList $Exists $StringToBool', lambda (strlist_, exists_, func_): ('.any', ('.map', func_, strlist_))),
+
+
+        # applying inverted $StringToBool functions
+    Rule('$Bool', '$StringToBool $String', lambda (func_, str_): ('.call', func_, str_)),
+    Rule('$Bool', '$StringToBool $String $Not', lambda (func_, str_, not_): (not_, ('.call', func_, str_))),
+    Rule('$Bool', '$StringToBool $StringListOr', lambda (func_, strlist_): ('.any', ('.map', func_, strlist_))),
+    Rule('$Bool', '$StringToBool $StringListAnd', lambda (func_, strlist_): ('.all', ('.map', func_, strlist_))),
+    Rule('$BoolList', '$StringToBool $StringList ', lambda (func_, strlist_): ('.map', func_, strlist_)),
+    Rule('$Bool', '$StringToBool $Exists $StringList ', lambda (func_, exists_, strlist_): ('.any', ('.map', func_, strlist_))),
 
         # defining $StringToBool functions
     Rule('$StringToBool', '$UnaryStringToBool', lambda sems: (sems[0],)),
@@ -204,6 +231,9 @@ compositional_rules = [
     Rule('$PhraseList', '$Between $ArgXAnd', lambda (btw_, arglist_): (btw_, arglist_)),
     Rule('$PhraseList', '$Sentence', lambda (sent,): (sent,)),
 
+        # inverted directions
+    Rule('$PhraseList', '$ArgX $Direction', lambda (arg_, dir_): (flip_dir(dir_), arg_)),
+
         # implicit 'in'
     # Rule('$StringToBool', '$StringAndArgToBool $ArgX', sems_in_order),
     # Rule('$StringToBool', '$StringAndArgToBool $ArgXOr', lambda sems: ('.composite_or', (sems[0],), sems[1])),
@@ -212,15 +242,26 @@ compositional_rules = [
             # "is left of Y"
     Rule('$StringToBool', '$Direction $ArgX',
         lambda (dir_, arg_): ('.in', ('.extract_text', (dir_, arg_)))),
-            # "is two words left of Y"    
+
+            # "is two words left of Y"
     Rule('$StringToBool', '$Int ?$Unit $Direction $ArgX', 
         lambda (int_, unit_, dir_, arg_): ('.in', ('.extract_text', 
             (dir_, arg_, ('.string', '.eq'), int_, ('.string', (unit_ if unit_ else 'words')))))),
+            # "X is immediately before"    
+    Rule('$StringToBool', '$ArgX $Int ?$Unit $Direction', 
+        lambda (arg_, int_, unit_, dir_): ('.in', ('.extract_text', 
+            (flip_dir(dir_), arg_, ('.string', '.eq'), int_, ('.string', (unit_ if unit_ else 'words')))))),
             # "is at least 40 words to the left of"
+    
     Rule('$StringToBool', '$Compare $Int ?$Unit $Direction $ArgX', 
         lambda (cmp_, int_, unit_, dir_, arg_): ('.in', ('.extract_text', 
             (dir_, arg_, ('.string', cmp_), int_,('.string', (unit_ if unit_ else 'words')))))), 
+            # "is to the left of Y by at least 40 words"
+    Rule('$StringToBool', '$Direction $ArgX $Compare $Int ?$Unit', 
+        lambda (dir_, arg_, cmp_, int_, unit_): ('.in', ('.extract_text', 
+            (dir_, arg_, ('.string', cmp_), int_,('.string', (unit_ if unit_ else 'words')))))), 
             # "between X and Y"
+    
     Rule('$StringToBool', '$Between $ArgXAnd', 
         lambda (btw_, arglist_): 
             ('.in', ('.extract_text', (btw_, arglist_)))), 
@@ -269,7 +310,9 @@ compositional_rules = [
     Rule('$StringList', '$PhraseList', lambda sems: ('.extract_text', sems[0])),
 
     Rule('$String', '$ArgToString $ArgX', lambda (func_, arg_): ('.call', func_, arg_)),
+    Rule('$String', '$ArgX $ArgToString', lambda (arg_, func_): ('.call', func_, arg_)),
     Rule('$StringListAnd', '$ArgToString $ArgXAnd', lambda (func_, args_): ('.map', func_, args_)),
+    Rule('$StringListAnd', '$ArgXAnd $ArgToString', lambda (args_, func_): ('.map', func_, args_)),
     Rule('$StringTuple', '$Tuple $StringListAnd', sems_in_order),
     Rule('$TupleToBool', '$In $List', sems_in_order),
     Rule('$Bool', '$StringTuple $TupleToBool', lambda (tup_, func_): ('.call', func_, tup_)),
@@ -347,8 +390,8 @@ snorkel_ops = {
 def sem_to_str(sem):
     str_ops = {
         '.root': lambda LF: recurse(LF),
-        '.label': lambda label, cond: "return {} if {} else 0".format(1 if recurse(label)==True else -1, recurse(cond)),
-        '.bool': lambda bool_: bool(bool_),
+        '.label': lambda label, cond: "return {} if {} else 0".format(1 if recurse(label)=='True' else -1, recurse(cond)),
+        '.bool': lambda bool_: bool_=='True',
         '.string': lambda str_: "'{}'".format(str_),
         '.int': lambda int_: int(int_),
         
@@ -365,24 +408,33 @@ def sem_to_str(sem):
         '.any': lambda x: "any({})".format(recurse(x)),
         '.none': lambda x: "not any({})".format(recurse(x)),
 
+        # '.composite_or': lambda func_, list_: "any({}({}))".format(recurse(func_), recurse(list_)),
+
+        '.geq': lambda x: "(>= {})".format(recurse(x)),
+
         '.arg': lambda int_: "arg{}".format(int_),
         '.arg_to_string': lambda arg_: "text({})".format(recurse(arg_)),
         '.cid': lambda arg_: "cid({})".format(recurse(arg_)),
 
         '.in': lambda rhs: "in {}".format(recurse(rhs)),
-        '.contains': lambda rhs: "contains {}".format(recurse(rhs)),
+        '.contains': lambda rhs: "contains({})".format(recurse(rhs)),
+        '.count': lambda list_: "count({})".format(recurse(list_)),
+        '.sum': lambda arg_: "sum({})".format(recurse(arg_)),
 
         '.between': lambda list_: "between({})".format(recurse(list_)),
         '.right': lambda *args_: "right({})".format(','.join(recurse(x) for x in args_)),
+        '.left': lambda *args_: "left({})".format(','.join(recurse(x) for x in args_)),
+        '.sentence': "sentence",
 
+        '.filter_to_tokens': lambda list_: "tokens({})".format(list_),
         '.extract_text': lambda list_: "text({})".format(list_),
     }
     def recurse(sem):
         if isinstance(sem, tuple):
             if sem[0] in str_ops:
                 op = str_ops[sem[0]]
-                args = [recurse(arg) for arg in sem[1:]]
-                return op(*args) if args else op
+                args_ = [recurse(arg) for arg in sem[1:]]
+                return op(*args_) if args_ else op
             else:
                 return str(sem)
         else:
